76f1e0c6120416fa87ee9b42f59aab6afe19fd06
Yumi Yukimura <me.cafebabe@gmail.com>
2025-09-24 23:04:55 +0800

    build: Disable `aosp_system_image` soong target for hertzify GSI
    Change-Id: I2cdb1f3cf1a00ba8fad7b8fc0745ed174a32a068
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- target/product/gsi_release.mk
+++ target/product/gsi_release.mk
@@ -27,6 +27,9 @@
 #
 
 BUILDING_GSI := true
+ifneq ($(filter hertzify_gsi_%,$(TARGET_PRODUCT)),)
+BUILDING_HERTZIFY_GSI := true
+endif
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/etc/init/config \
